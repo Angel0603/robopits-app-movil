cffa4b3300dba0d219b3eddb6ab98b7656a3e905
Angel0603 <20211050@uthh.edu.mx>
2024-10-23 18:55:25 -0600

    Se agrega archivo de configuracion de Github Actions test.yaml

--- views/Login.jsx
+++ views/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    
 
     const togglePasswordVisibility = () => {
         setIsPasswordVisible(!isPasswordVisible);
