904278d6cbc68f7dd5f3acbc7a7a8c1a6f6ec347
Angel0603 <20211050@uthh.edu.mx>
2024-10-29 08:32:42 -0600

    Se actualizó Login.test.jsx

--- views/__test__/Login.test.jsx
+++ views/__test__/Login.test.jsx
@@ -7,36 +7,42 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper'); // Para evita
 
 
 describe('Login', () => {
-  it('Funcionamiento correcto del formulario para iniciar sesión', async () => {
-    const mockFetch = jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue({ message: 'Login successful' }),
-      status: 200,
-    });
+  beforeAll(() => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ message: 'Login successful' }),
+        status: 200,
+      })
+    );
+  });
 
+  afterAll(() => {
+    delete global.fetch; // Limpiar el mock después de las pruebas
+  });
+
+  it('Funcionamiento correcto del formulario para iniciar sesión', async () => {
     render(<Login />);
 
     const emailInput = screen.getByText(/Ingrese su email/i);
     const passwordInput = screen.getByText(/Ingrese su contraseña/i);
     const loginButton = screen.getByText(/Iniciar sesión/i);
 
-    // Simular ingreso de texto en los campos
     fireEvent.changeText(emailInput, '20211050@uthh.edu.mx');
     fireEvent.changeText(passwordInput, 'Ang#1324#');
 
-    // Simular clic en el botón de login
     fireEvent.press(loginButton);
 
     await waitFor(() => {
-      expect(mockFetch).toHaveBeenCalledWith('https://back-end-robopits.vercel.app/api/login', expect.objectContaining({
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ Email: '20211050@uthh.edu.mx', Password: 'Ang#1324#' }),
-      }));
+      expect(global.fetch).toHaveBeenCalledWith(
+        'https://back-end-robopits.vercel.app/api/login',
+        expect.objectContaining({
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ Email: '20211050@uthh.edu.mx', Password: 'Ang#1324#' }),
+        })
+      );
     });
-
-    // Limpiar el mock
-    mockFetch.mockRestore();
   });
 });
